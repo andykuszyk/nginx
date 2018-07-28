527ccb08f2e5c9e441cde45cf4bdeb31331e1ae5
andykuszyk <andy.kuszyk@carfinance247.co.uik>
2018-07-28 17:19:01 +0100

    Added deployment of nginx

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@ commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
 docker build -t andykuszyk/nginx:$commit .
 docker push andykuszyk/nginx:$commit
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
+scp -i id_rsa.pub $douser:$doip ./docker-compose.yml:~/docker-compose.yml
+ssh -i id_rsa.pub -o "StrictHostKeyChecking no" $douser:$doip "docker stack deploy -c ~/docker-compose.yml szyk" 
