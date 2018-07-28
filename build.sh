624dd135ea93091b4e53ff7b31ca17441791aa2f
Andy Kuszyk <andykuszyk@live.co.uk>
2018-07-28 18:40:25 +0100

    Corrected use of scp

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ commit=`git log -n 1 --oneline | sed 's/\(.......\).*/\1/'`
 docker build -t andykuszyk/nginx:$commit .
 docker push andykuszyk/nginx:$commit
 sed -i "s/\[version\]/$commit/g" docker-compose.yml
-scp -i id_rsa ./docker-compose.yml $douser@$doip:~/docker-compose.yml
+scp -i id_rsa -o "StrictHostKeyChecking no" ./docker-compose.yml $douser@$doip:~/docker-compose.yml
 ssh -i id_rsa -o "StrictHostKeyChecking no" $douser@$doip "docker stack deploy -c ~/docker-compose.yml szyk" 
